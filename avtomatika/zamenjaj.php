687b1bd5cd4b7f9114cbbaed3a0d8a4ce10a571b
Jure Sah <jure.sah@denia.si>
2012-10-28 19:33:43 +0100

    seach-replace prevajanje

---
+++ avtomatika/zamenjaj.php
@@ -0,0 +1,34 @@
+<?php
+
+//VSEBUJE $username IN $password ZA BAZO 
+include "../../dbcreds.php";
+
+$sitePot = "../sl-SI_joomla_lang_full_3.0.1v1/site_sl-SI";
+$adminPot = "../sl-SI_joomla_lang_full_3.0.1v1/admin_sl-SI";
+$site = scandir($sitePot);
+$admin = scandir($adminPot);
+
+$con = mysql_connect("127.0.0.1",$username,$password);
+mysql_select_db("prevod");
+
+foreach($site as $file) {
+ if(substr($file,-4)==".ini") {
+  $vsebina = file_get_contents($sitePot."/".$file);
+
+  $r = mysql_query("
+   SELECT * FROM zamenjaj;
+  ");
+  $n = mysql_num_rows($r);
+
+  for($i=0; $i<$n; $i++) {
+   $v = mysql_fetch_assoc($r);
+   $vsebina = str_replace($v['kaj'],$v['sCim'],$vsebina);
+  }
+  
+  echo $vsebina;
+ }
+}
+
+mysql_close($con);
+
+?>
